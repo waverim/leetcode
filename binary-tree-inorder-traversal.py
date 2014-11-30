706e3f5f7705dff6b6e6b448ba22065869d3fcf0
Waverim <waverimax@gmail.com>
2014-11-30 14:46:54 +0800

    add inorder, change preorder's comments

--- binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal.py
@@ -1,9 +1,10 @@
 """
-二叉树非递归先序遍历，用一个栈来暂存节点
+二叉树非递归中序遍历，与先序遍历思路差不多
+用一个栈来暂存节点
 首先如果节点为空 并且 栈也为空，则遍历结束
-否则：如果节点不为空，则输出节点信息，判断：如果该节点有右子节点，则该节点的右子节点入栈
-否则：查询该节点的左子节点（默认左子节点存在，下一个循环判断）
-如果该节点为空，则该空节点的父节点无左子树，将栈顶的右子节点出栈，循环判断该右子节点这棵树
+否则：如果节点不为空，则查询该节点的左子节点（默认左子节点存在，下一个循环判断）
+否则：该节点为空，则该空节点的父节点无左子树，将栈顶的父节点出栈，输出节点信息
+遍历右子节点
 如此循环，直至栈为空或者节点也为空
 """
 
@@ -17,19 +18,19 @@ class TreeNode:
 class Solution:
     # @param root, a tree node
     # @return a list of integers
-    def preorderTraversal(self, root):
+    def inorderTraversal(self, root):
         stack = []
         result = []
         while root is not None or len(stack) != 0:
             if root is not None:
-                result.append(root.val)
-                if root.right is not None:
-                    stack.append(root.right)
+                stack.append(root)
                 root = root.left
             else:
                 root = stack.pop()
+                result.append(root.val)
+                root = root.right
         return result
-    
+
 # test
 root = TreeNode(0)
 node1 = TreeNode(1)
@@ -41,5 +42,5 @@ root.left = node1
 root.right = node2
 node1.left = node3
 node1.right = node4
-print Solution().preorderTraversal(root)
-print Solution().preorderTraversal(None)
+print Solution().inorderTraversal(root)
+print Solution().inorderTraversal(None)
